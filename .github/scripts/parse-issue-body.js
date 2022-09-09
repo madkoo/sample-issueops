a7b86545ca5f2f9047210208ff286d5ef040b498
Madis Kõosaar <madis.koosaar@solidify.dev>
2022-09-09 18:40:54 +0300

    Create parse-issue-body.js

---
+++ .github/scripts/parse-issue-body.js
@@ -0,0 +1,23 @@
+module.exports = ({context, core}) => {
+    const issueBody = context.payload.issue.body;
+
+    const parsedWhoToGreet = issueBody.match(/### Hello!\s+(?<whoToGreet>[^\s]+)\s/);
+    const parsedMutliLine = issueBody.match(/### Large area for text\s+```csv(?<multiLine>[^`]+)```\s/);
+
+    if (core) {
+      if (parsedWhoToGreet) {
+        core.setOutput('who-to-greet', parsedWhoToGreet.groups.whoToGreet);
+      }
+       if (parsedMutliLine) {
+        core.setOutput('mutliline-value', parsedMutliLine.groups.multiLine.trim());
+      }
+    }
+  
+    let result = {
+      whoToGreet: parsedWhoToGreet.groups.whoToGreet,
+      multiLine: parsedMutliLine.groups.multiLine
+      
+    };
+  
+    return result;
+  }
